6b6a12d6a8fdb591a5a48b324da3b46101c4529e
Macbook <macbook@macbooks-air.dlink>
2018-03-29 01:21:38 +0200

    Initial Commit

---
+++ Splash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Splash
+//
+//  Created by Ivan Glushko on 29.03.18.
+//  Copyright © 2018 ivanglushko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
